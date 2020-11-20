71e862746a52b181947694af46e955d095593f16
chentaolu <56255406+chentaolu@users.noreply.github.com>
2020-11-20 21:51:54 +0800

    test unity folder

---
+++ Assets/playercootion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class playercootion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public playermove playermove;
+
+    private void OnCollisionEnter(Collision collision) 
+    {
+        Debug.Log(collision.collider.tag);
+        if (collision.collider.tag == "WALL")
+        {
+            playermove.enabled = false;
+        }
+    }
+}
---
+++ Assets/playermove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwordforce = 2000f;
+    public float sideForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, forwordforce * Time.deltaTime);
+        
+        
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
